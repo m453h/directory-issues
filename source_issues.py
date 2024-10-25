40a4139380323bc2c1fd1e768b38493fc6dda0ef
Paige Gulley <pgulley@campus-vpn-172-26-69-106.vpn.umass.edu>
2024-10-25 17:31:08 -0400

    added source issue template

--- source_issues.py
+++ source_issues.py
@@ -1,7 +1,18 @@
 from issues import IssueBase
 from typing import TypeVar, Tuple, Generic, Type, Dict, Callable, List, Any
 import datetime as dt
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from collections import defaultdict
 
+
+###Eventually manage this via pkg_resources
+import os
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+
+jinja_env = Environment(
+    loader=FileSystemLoader(template_dir),
+    autoescape=select_autoescape()
+)
 
 class Source():
     
@@ -23,15 +34,35 @@ class Source():
         self.media_type: str = data.get("media_type")
         self.collection_count: int = data.get("collection_count")
 
+        self.link = f"https://search.mediacloud.org/sources/{self.id}"
+        
         #This will also contain a mechanism for searching against this source to get a volume over time value
         #which can also be used as a metric input
 
+        self.issues = None
 
     def __repr__(self):
         return f"Source({self.name}, {self.homepage})"
 
     def find_issues(self, include_tags=None, exclude_tags=None):
         self.issues = SourceIssue.calculate_all(self, include_tags=include_tags, exclude_tags=exclude_tags)
+
+        grouped_issues = defaultdict(list)
+        for issue in self.issues:
+            primary_tag = issue['tags'][0] if issue['tags'] else 'no_tag'
+            grouped_issues[primary_tag].append(issue['template'])
+        self.grouped_issues = grouped_issues
+
+    def render_template(self):
+        if not self.issues:
+            self.find_issues()
+
+        datestr = dt.datetime.now().strftime("%I:%M%p on %B %d, %Y")
+
+        source_message_template = jinja_env.get_template("source_issues_message.j2")
+
+        source_message = source_message_template.render(source=self, run_date=datestr)
+        return source_message
 
 
 class SourceIssue(IssueBase["Source"]):
