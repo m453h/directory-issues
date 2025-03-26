8b7f952a0511be0892d91f57f8a67e11fe05ae79
Michael Hudson Nkotagu <mnkotagu@gmail.com>
2025-03-26 07:33:59 +0300

    Implement script to validate that sources are in correct state collection based on pub_state value

--- directory_issues/scripts/sources/base.py
+++ directory_issues/scripts/sources/base.py
@@ -1,3 +1,5 @@
+import csv
+
 import pandas as pd
 import logging
 from typing import Optional, Any, Dict, List
@@ -79,3 +81,62 @@ class SourcesBase:
 
     def analyze_source(self, domain: str):
         raise NotImplementedError("Subclasses must implement this method.")
+
+
+class CollectionsBase:
+    def __init__(self, client: MediaCloudClient):
+        self.client = client
+        self.states = {
+            "Alabama": "AL", "Alaska": "AK", "Arizona": "AZ", "Arkansas": "AR", "California": "CA", "Colorado": "CO",
+            "Connecticut": "CT",
+            "Delaware": "DE", "Florida": "FL", "Georgia": "GA", "Hawaii": "HI", "Idaho": "ID", "Illinois": "IL",
+            "Indiana": "IN",
+            "Iowa": "IA", "Kansas": "KS", "Kentucky": "KY", "Louisiana": "LA", "Maine": "ME", "Maryland": "MD",
+            "Massachusetts": "MA",
+            "Michigan": "MI", "Minnesota": "MN", "Mississippi": "MS", "Missouri": "MO", "Montana": "MT",
+            "Nebraska": "NE", "Nevada": "NV",
+            "New Hampshire": "NH", "New Jersey": "NJ", "New Mexico": "NM", "New York": "NY", "North Carolina": "NC",
+            "North Dakota": "ND",
+            "Ohio": "OH", "Oklahoma": "OK", "Oregon": "OR", "Pennsylvania": "PA", "Rhode Island": "RI",
+            "South Carolina": "SC", "South Dakota": "SD",
+            "Tennessee": "TN", "Texas": "TX", "Utah": "UT", "Vermont": "VT", "Virginia": "VA", "Washington": "WA",
+            "West Virginia": "WV", "Wisconsin": "WI",
+            "Wyoming": "WY", "District of Columbia": "DC"
+        }
+
+        self.collection_lookup = {}  # Used to determine the collection_id of a given state collection e.g. [Massachusetts, United States - State & Local] -> [ US-MA ] -> collection_id ?
+        self.states_code_lookup = {}  # Used to determine the state code for a given state name e.g. [Massachusetts] -> [US-MA]
+        self.sources_lookup = {}  # Used to determine the collection ids of a given source e.g. [Source ID] -> Collection_ids ?
+
+    def create_lookup_data(self):
+        collections = self.client.directory_client.collection_list(name="United States")["results"]
+        for collection in collections:
+            name_part = collection["name"].split(
+                ",")  # Split strings like Massachusetts, United States - State & Local to get state name
+            if len(name_part) > 1 and (
+                    "United States - State & Local" in name_part[1] or "United States -State & Local" in name_part[1]):
+                state_code = f"US-{self.states[name_part[0]]}"
+                if self.states[name_part[0]]:
+                    self.collection_lookup[state_code] = collection["id"]
+                    self.states_code_lookup[collection["id"]] = state_code
+
+                    # Get sources under a collection and build the sources lookup dict
+                    collection_sources = \
+                    self.client.directory_client.source_list(collection_id=collection["id"], limit=collection["source_count"])[
+                        "results"]
+                    for source in collection_sources:
+                        # The assumption is a source can appear in more than one collection
+                        if source["id"] in self.sources_lookup:
+                            self.sources_lookup[source["id"]].append(collection["id"])
+                        else:
+                            self.sources_lookup[source["id"]] = [collection["id"]]
+
+    def write_output(self, file_name, data):
+        with open(file_name, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['source_id', 'label', 'homepage','pub_state','correct_collection_id']),
+            for item in data:
+                writer.writerow([item['id'], item['label'], item['homepage'],item['pub_state'], self.collection_lookup.get(item['pub_state'])])
+
+    def run_process(self):
+        raise NotImplementedError("Subclasses must implement this method")
